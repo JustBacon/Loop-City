12b39fcf3b9aef6852e9245c51abd9e91b50806a
Christian Buco <christianbuco17@gmail.com>
2016-11-28 08:43:38 -1000

    loop

---
+++ loopcity.js
@@ -0,0 +1,32 @@
+// mommy can count to 100
+var count ;
+
+for (var count = 0; count <= 100; count++) {
+	console.log("banana " + count);
+}
+
+// Noah Arc
+var animalCount ;
+
+for (var animalCount = 0; animalCount <= 5 ; animalCount++) {
+	console.log("there are " + animalCount + " animals in Noahs Arc");
+}
+
+// Hip Hop Array
+var partyAnimals = [1,2,3,4,5];
+
+for (var a = 0; a <= 4; a++) {
+	console.log(partyAnimals[a]);
+}
+
+// Merlin's Battle Weapon
+var weaponChest = ["Mace", "Sorcerer's Stone", "Excalibur"];
+var hero = {
+	wizard = Merlin
+	weapon = weaponChest[2]
+	power = 30
+}
+
+function stuff(a){
+	if()
+}
