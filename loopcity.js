ffca098b9e068c8d5f66fc9a63d3037784af8778
Christian Buco <christianbuco17@gmail.com>
2016-11-30 08:52:40 -1000

    loop

--- loopcity.js
+++ loopcity.js
@@ -22,11 +22,17 @@ for (var a = 0; a <= 4; a++) {
 // Merlin's Battle Weapon
 var weaponChest = ["Mace", "Sorcerer's Stone", "Excalibur"];
 var hero = {
-	wizard = Merlin
-	weapon = weaponChest[2]
-	power = 30
+	wizard : 'Merlin',
+	weapon : weaponChest[2],
+	power : 30
 }
 
 function stuff(a){
-	if()
+	if(a == "Sorcerer's stone"){
+		console.log(hero.wizard + ' shall slay the dragon with the ' + hero.weapon);
+	}else{
+		hero.weapon = weaponChest[1]
+		console.log(hero.wizard + ' is ready for war with the ' + hero.weapon);
+	}
 }
+stuff(hero.weapon)
